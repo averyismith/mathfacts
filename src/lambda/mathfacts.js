e35ad4891a89735dd27d8f9823fad0fd65588f0e
jonasmit <jon.a.smith@gmail.com>
2020-03-16 15:03:35 -0700

    up

--- src/lambda/mathfacts.js
+++ src/lambda/mathfacts.js
@@ -1,17 +1,13 @@
 export async function handler() {
-    var min = Math.ceil(1);
-    var max = Math.floor(99); 
-
-    var rmin = Math.ceil(1);
-    var rmax = Math.floor(99);  
-
+    var x = randomInteger(1, 10);
+    var y = randomInteger(1, 10);
+   
   return {
     statusCode: 200,
-    body: JSON.stringify(
-        Operands = {
-            left = Math.floor(Math.random() * (max - min + 1)) + min,
-            right = Math.floor(Math.random() * (rmax - rmin + 1)) + rmin
-        }
-    )
+    body: JSON.stringify({x: x, y: y, p: x*y})
   };
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
